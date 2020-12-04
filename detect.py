17ca5bfa17700fb1724b8c1d9c673ff2d06133de
bdizon <59716901+bdizon@users.noreply.github.com>
2020-12-04 11:41:46 -0500

    changes in parameter

--- detect.py
+++ detect.py
@@ -6,6 +6,7 @@ Created on Sun Apr  5 15:44:42 2020
 """
 import sys
 sys.path.append("./model/")
+import os
 from PIL import Image, ImageDraw, ImageFont
 from utils import *
 from torchvision import transforms
@@ -16,19 +17,33 @@ import argparse
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-ap = argparse.ArgumentParser()
-ap.add_argument("--trained_model", default = "model_state_ssd300.pth.tar", type= str,
-                help = "Trained state_dict file path to open")
-ap.add_argument("--input", type= str, help= "Input path for detect")
-ap.add_argument("--output", type= str, help = "Output path to save")
-ap.add_argument("--min_score", default= 0.4, type= float, help = "Min score for NMS")
-ap.add_argument("--max_overlap", default= 0.45, type= float, help = "Max overlap for NMS")
-ap.add_argument("--top_k", default= 200, type= int, help = "Top k for NMS")
-args = ap.parse_args()
+# ap = argparse.ArgumentParser()
+# ap.add_argument("--trained_model", default = "model_state_ssd300.pth.tar", type= str,
+#                 help = "Trained state_dict file path to open")
+# ap.add_argument("--input", type= str, help= "Input path for detect")
+# ap.add_argument("--output", type= str, help = "Output path to save")
+# ap.add_argument("--min_score", default= 0.4, type= float, help = "Min score for NMS")
+# ap.add_argument("--max_overlap", default= 0.45, type= float, help = "Max overlap for NMS")
+# ap.add_argument("--top_k", default= 200, type= int, help = "Top k for NMS")
+# args = ap.parse_args()
 
-img_path = args.input
-trained_model = torch.load(args.trained_model)
-ouput_path = args.output
+# input_path = "images_part1/000001.png"
+# input_path = "images_part1/035435.png"
+input_path = "images_part1/000079.png"
+# img_path = args.input
+img_path = input_path
+saved_model = "model_state_ssd300_epoch0.pth.tar"
+trained_model = torch.load(saved_model)
+# trained_model = torch.load(args.trained_model)
+# output_path = "results/35435.jpeg"
+output_path = "results/000079.png"
+# ouput_path = args.output
+min_score = 0.0001
+max_overlap = 0.1
+top_k = 200
+
+
+# start_epoch = trained_model["epoch"] + 1
 start_epoch = trained_model["epoch"] + 1
 print('\nLoaded model trained with epoch %d.\n' % start_epoch)
 model = trained_model['model']
@@ -59,8 +74,10 @@ def detect(original_image, min_score, max_overlap, top_k, suppress = None):
     detect_boxes = detect_boxes * original_dims
     
     detect_labels = [rev_label_map[l] for l in detect_labels[0].to('cpu').tolist()]
+    print(detect_labels)
     
     if detect_labels == ["background"]:
+        print("original image returned")
         return original_image
     
     annotated_image = original_image
@@ -89,9 +106,32 @@ def detect(original_image, min_score, max_overlap, top_k, suppress = None):
     return annotated_image
 
 if __name__ == '__main__':
+    # for i in range(90):
+    #     input_path = "images_part1/0000" + str(10+i) + ".png"
+    #     img_path = input_path
+    #     output_path = "results/0000" + str(i+1) + ".png"
+    directory = "images_part1"
+    output_directory = "results"
+    num = 0
+    # for filename in os.listdir(directory):
+    #     if filename.endswith(".png") and num <100:
+    #         print(os.path.join(directory, filename))
+    #         img_path = os.path.join(directory, filename)
+    #         output_path = os.path.join(output_directory, filename)
+    #         print(output_path)
+    #         original_image = Image.open(img_path, mode='r')
+    #         original_image = original_image.convert('RGB')
+    #         annotated_image = detect(original_image, min_score=min_score, 
+    #                                 max_overlap=max_overlap, top_k= top_k)
+    #         annotated_image.save(output_path)
+    #         # annotated_image.show()
+            # num += 1
+
+    img_path = "images_part1/028302.png"
+    output_path = "results/028302.png"
     original_image = Image.open(img_path, mode='r')
     original_image = original_image.convert('RGB')
-    annotated_image = detect(original_image, min_score=args.min_score, 
-                             max_overlap=args.max_overlap, top_k= args.top_k)
-    annotated_image.save(ouput_path)
-    annotated_image.show()
+    annotated_image = detect(original_image, min_score=min_score, 
+                            max_overlap=max_overlap, top_k= top_k)
+    annotated_image.save(output_path)
+        # annotated_image.show()
